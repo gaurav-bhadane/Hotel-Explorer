7c93e5444f28cbf2c722400e5649afc2bb42ddf7
gaurav-bhadane <gauravpbhadane2304@gmail.com>
2024-02-09 17:27:36 +0530

    Client-side Validation Commited

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(methodOverride('_method'))
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"/views/listing"))
 app.use(express.static(path.join(__dirname,"public/css")))
+app.use(express.static(path.join(__dirname,"public/js")))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
