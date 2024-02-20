e74918dfb913fbfbe2e5333ff5ceeb7505bec026
gaurav-bhadane <gauravpbhadane2304@gmail.com>
2024-02-20 19:01:59 +0530

    Signup Functionality Created

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs")
 })
 
-router.post("/signup",wrapAsync(async(req,res)=>{
+router.post("/signup",wrapAsync(async(req,res,next)=>{
     try{
         const {username,email,password}=req.body;
         const newUser = new User ({
@@ -18,8 +18,14 @@ router.post("/signup",wrapAsync(async(req,res)=>{
         });
         let registeredUser=await User.register(newUser,password)
         console.log(registeredUser);
-        req.flash("success","Welcome to Wanderlust")
-        res.redirect("/listings")
+        req.login(registeredUser,(err)=>{
+            if (err){
+                return next(err)
+            }
+            req.flash("success","Welcome to Wanderlust")
+            res.redirect("/listings")
+        })
+        
     }catch(e){
         req.flash("error",e.message)
         res.redirect('/signup')
