33e05b11e84c0a966178239e8bb1b0ba04ff9411
gaurav-bhadane <gauravpbhadane2304@gmail.com>
2024-02-20 18:55:12 +0530

    User register Styled

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req,res,next)=>{
+    console.log(req.user)
     if (!req.isAuthenticated()){
         req.flash("error","You must be logged in to Create Listing")
         return res.redirect('/login')
